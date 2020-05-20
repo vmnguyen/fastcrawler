d3a17d01c969aafad7c88e6d780f54a80ab50c82
vmnguyen <manhnguyen510@gmail.com>
2020-05-20 18:13:39 +0700

    Add code process request and new function for process response

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-# fastcrawl
-Super fast http crawl written in Go
+# fastcrawler
+Super fast http crawler written in Go
 
-'''
+# Usage
+```
 Usage of ./fastcrawl:
   -c int
         Concurency number (default 50)
   -t string
         Target to scan (default "https://example.com")
+```
 
-'''
+# Example
+
+```./fastcrawl -t https://example.com -c 50```
--- main.go
+++ main.go
@@ -21,9 +21,22 @@ var (
 func doRequest(i interface{}) {
 	// TODO
 	// Add code for make request to target
-	url := i.(int)
+	url := i.(string)
 	fmt.Println("Send request to target:", url)
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseRequest(req)
+	defer fasthttp.ReleaseResponse(resp)
 
+	req.SetRequestURI(url)
+	client.Do(req, resp)
+	bodyByte := resp.Body()
+	fmt.Println(string(bodyByte))
+
+}
+
+func getAllLink(respondBody string) {
+	return
 }
 
 func runScan(target string, concurency int) {
@@ -34,20 +47,19 @@ func runScan(target string, concurency int) {
 		wg.Done()
 	})
 	defer p.Release()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
-		_ = p.Invoke(i)
+		_ = p.Invoke(target)
 	}
 }
 
 func main() {
 	var target string
-	flag.StringVar(&target, "t", "https://example.com", "Target to scan")
+	flag.StringVar(&target, "t", "http://demo.testfire.net", "Target to scan")
 
 	var concurency int
 	flag.IntVar(&concurency, "c", 50, "Concurency number")
 
 	flag.Parse()
 	runScan(target, concurency)
-
 }
